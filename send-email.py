eccabef28c4c53c4f1167e229934b6949bfca272
zhoujingya <jing.zhou@terapines.com>
2023-02-08 23:43:58 +0800

    Update send email

---
+++ send-email.py
@@ -0,0 +1,27 @@
+from email.message import EmailMessage
+from password import password
+import ssl
+import smtplib
+
+email_sender = "zhoujingfightinghahaha@gmail.com"
+email_password = password
+
+email_receiver = "254644528@qq.com"
+
+subject = "This is test"
+body = """
+This is a hello from zhoujing
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+  smtp.login(email_sender, email_password)
+  smtp.sendmail(email_sender, email_receiver, em.as_string())
+
