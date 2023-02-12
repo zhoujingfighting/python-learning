56d5b034741bdadbcc7d487f092677d446c8864c
zhoujing <jing.zhou@terapines.com>
2023-02-12 19:06:10 +0800

    Update dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+from PyDictionary import PyDictionary
+
+dictioanry = PyDictionary("eyes")
+
+# while True:
+#     word=input("Enter your word: ")
+#     if word == "":
+#         break
+
+#     print(dictioanry.meaning(input))
+
+print(dictioanry.getMeanings())
