e5a1b559742cd39d7c3771d64a2d26917916259f
zhoujingfighting <jing.zhou@terapines.com>
2023-02-13 22:06:27 +0800

    Update shecdule

--- automation.py
+++ automation.py
@@ -4,6 +4,8 @@ schedule requests
 """
 
 import requests
+import schedule
+import time
 
 def send_message():
     resp = requests.post('https://textbelt.com/text', {
@@ -14,5 +16,10 @@ def send_message():
 
     print(resp.json())
 
-send_message()
+# schedule.every().day.at("10:30").do(send_message)
+# .at('06.00').do(send_message)
+schedule.every(10).seconds.do(send_message)
 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
